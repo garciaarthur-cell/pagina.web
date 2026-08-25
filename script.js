fa8176de682d514f9e0f34c8c5d409180b5c4a3b
garciaarthur-cell <garcia.arthur@escola.pr.gov.br>
2026-08-25 09:35:19 -0300

    botão?

--- script.js
+++ script.js
@@ -5,15 +5,6 @@ let texto = botao.querySelector("span");
 if (curtiu === false) {
 texto.textContent++;
 curtiu = true;
-}
-}
-function botaoClicado() {
-console.log("fui clicado");
-let texto = botao.querySelector("span");
-
-if (curtiu === false) {
-texto.textContent++;
-curtiu = true;
 } else {
 texto.textContent--;
 curtiu = false;
