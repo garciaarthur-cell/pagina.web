cd4381c99e3b5b2b6fd15ffc4a90bac47cccf9ee
garciaarthur-cell <garcia.arthur@escola.pr.gov.br>
2026-08-25 09:36:35 -0300

    botãos2

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
+botoes.forEach(function(botao) {
+botao.addEventListener("click", botaoClicado);
+
 function botaoClicado() {
 console.log("fui clicado");
 let texto = botao.querySelector("span");
-
-if (curtiu === false) {
 texto.textContent++;
-curtiu = true;
-} else {
-texto.textContent--;
-curtiu = false;
 }
-}
+});
