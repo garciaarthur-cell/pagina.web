19d53a7471b03cd336807383e23cc929400714a3
garciaarthur-cell <garcia.arthur@escola.pr.gov.br>
2026-08-27 17:30:28 -0300

    botão solo

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function botaoClicado() {
-console.log("fui clicado");
-let texto = botao.querySelector("span");
+const botoes = document.querySelectorAll("article button");
 
-if (curtiu === false) {
-texto.textContent++;
-curtiu = true;
-} else {
-texto.textContent--;
-curtiu = false;
-}
-}
+// cria a contagem individual para cada botão pois com os dois juntos a prog não tava funcionando //
+botoes.forEach((botao) => {
+    let curtiu = false;
+
+    botao.addEventListener("click", function botaoClicado() {
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
+
+        if (curtiu === false) {
+            texto.textContent = parseInt(texto.textContent) + 1;
+            curtiu = true;
+        } else {
+            texto.textContent = parseInt(texto.textContent) - 1;
+            curtiu = false;
+        }
+    });
+});
