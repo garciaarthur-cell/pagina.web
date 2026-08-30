6531cbaded651d8c42e45d7acd504c6d5dbb8f4a
garciaarthur-cell <garcia.arthur@escola.pr.gov.br>
2026-08-30 11:10:06 -0300

    muitas coisas foram alteradas

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const botoes = document.querySelectorAll("article button");
+const video = document.getElementById("vid1");
+const botao = document.getElementById("btao");
 
 // cria a contagem individual para cada botão pois com os dois juntos a prog não tava funcionando //
 botoes.forEach((botao) => {
-    let curtiu = false;
-
     botao.addEventListener("click", function botaoClicado() {
         console.log("fui clicado");
         let texto = botao.querySelector("span");
-
-        if (curtiu === false) {
-            texto.textContent = parseInt(texto.textContent) + 1;
-            curtiu = true;
-        } else {
-            texto.textContent = parseInt(texto.textContent) - 1;
-            curtiu = false;
-        }
+        texto.textContent = parseInt(texto.textContent) + 1;
     });
 });
+
+function abrir_opening() {
+
+    video.classList.remove("esconder");
+    video.play();
+    // Esconde o botão //
+    botao.style.display = "none";
+}
